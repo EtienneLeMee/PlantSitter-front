65d9806357a9069cc86169b8d26b696051546116
Etienne Le Mée <et.lemee@gmail.com>
2023-02-01 11:37:13 +0100

    Commit

--- plantsitter/src/views/DiscoverView.vue
+++ plantsitter/src/views/DiscoverView.vue
@@ -1,40 +1,38 @@
 <template>
-  <div class="discover">
-    <Discover/>
-  </div>
-  <div class="nav">
-    <Nav/>
-  </div>
-  
-  <title>Hogwarts Wiki</title>
+    <div class="discover">
+        <Discover />
+    </div>
+    <div class="nav">
+        <Nav />
+    </div>
+
+    <title>Hogwarts Wiki</title>
 </template>
 
 <script>
 // @ is an alias to /src
-import Discover from '@/components/Discover.vue'
-import Nav from '@/components/Nav.vue'
+import Discover from "@/components/Discover.vue";
+import Nav from "@/components/Nav.vue";
 
 export default {
-  name: 'DiscoverView',
-  components: {
-    Discover,
-    Nav
-  },
-  title:'Dis'
-}
+    name: "DiscoverView",
+    components: {
+        Discover,
+        Nav,
+    },
+    title: "Dis",
+};
 </script>
 
 <style>
-  nav {
+nav {
     position: absolute;
     z-index: 3;
     right: 0;
     margin-right: 40px;
-  }
+}
 
-  .home {
+.home {
     position: absolute;
-  }
-
-
+}
 </style>
